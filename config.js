4ef48a9d26c1b2e835720a76d8d89979f104c803
Gurrala Rishikesh <gurralarishikesh5@gmail.com>
2026-07-14 17:00:47 +0530

    Add configuration files for testing

---
+++ config.js
@@ -0,0 +1,17 @@
+window.APP_CONFIG_ENV = {
+    firebase: {
+        apiKey: "AIzaSyDGdYkt7_VXNWYCampFD4OW1POBCP2bITo",
+        authDomain: "school-accountant-15437.firebaseapp.com",
+        projectId: "school-accountant-15437",
+        storageBucket: "school-accountant-15437.firebasestorage.app",
+        messagingSenderId: "853042543438",
+        appId: "1:853042543438:web:104585e8e6f7b9e2657c1f",
+        measurementId: "G-DKJJJFKQJ9"
+    },
+    supabase: {
+        url: "https://vwqlfrfvsoanssazjtrg.supabase.co",
+        key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ3cWxmcmZ2c29hbnNzYXpqdHJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzNzk4MjIsImV4cCI6MjA5Nzk1NTgyMn0.l8QTZCbwxMmki5JMVflGtLA-etvLvc4k55DSnirI4V4",
+        table: "transactions",
+        bucket: "receipts"
+    }
+};
